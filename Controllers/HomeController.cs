54de54e7a641f9d321909ef4bc88e3e25376031a
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-02-11 19:09:01 +0000

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,14 +15,12 @@ namespace AMobile.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Description ";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
